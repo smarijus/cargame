bb286a4b11f1c9b8ad3144b83b57948986f730a4
Marijus Sebeckis <smarijus@yahoo.com>
2014-11-10 21:17:20 +0200

    pirmas komitas

---
+++ Assets/Scripts/CarControleScript.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+var wheelFL : WheelCollider;
+var wheelFR : WheelCollider;
+var wheelRL : WheelCollider;
+var wheelRR : WheelCollider;
+var maxTorque :  float = 50;
+
+function Start () {
+rigidbody.centerOfMass.y = -0.9;
+}
+
+function FixedUpdate () {
+wheelRR.motorTorque = maxTorque * Input.GetAxis("Vertical");
+wheelRL.motorTorque = maxTorque * Input.GetAxis("Vertical");
+wheelFL.steerAngle = 20 * Input.GetAxis("Horizontal");
+wheelFR.steerAngle = 20 * Input.GetAxis("Horizontal");
+
+}
