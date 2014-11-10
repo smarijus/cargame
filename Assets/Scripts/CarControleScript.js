97c9e3baecb0947d2666dc7a1518146f4e538d76
Programmer-M <wariuz.papildomas@gmail.com>
2014-11-10 22:12:01 +0200

    Commit bandymas
    Nieko gero

--- Assets/Scripts/CarControleScript.js
+++ Assets/Scripts/CarControleScript.js
@@ -5,14 +5,15 @@ var wheelRL : WheelCollider;
 var wheelRR : WheelCollider;
 var maxTorque :  float = 50;
 
-function Start () {
-rigidbody.centerOfMass.y = -0.9;
+function Start ()
+{
+	rigidbody.centerOfMass.y = -0.9;
 }
 
-function FixedUpdate () {
-wheelRR.motorTorque = maxTorque * Input.GetAxis("Vertical");
-wheelRL.motorTorque = maxTorque * Input.GetAxis("Vertical");
-wheelFL.steerAngle = 20 * Input.GetAxis("Horizontal");
-wheelFR.steerAngle = 20 * Input.GetAxis("Horizontal");
-
+function FixedUpdate ()
+{
+	wheelRR.motorTorque = maxTorque * Input.GetAxis("Vertical");
+	wheelRL.motorTorque = maxTorque * Input.GetAxis("Vertical");
+	wheelFL.steerAngle = 20 * Input.GetAxis("Horizontal");
+	wheelFR.steerAngle = 20 * Input.GetAxis("Horizontal");
 }
