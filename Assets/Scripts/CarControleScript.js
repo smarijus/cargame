622e28afa8f7bb8c8104a6f8bbbc06cfe0be7ca8
Marijus Sebeckis <smarijus@yahoo.com>
2014-11-11 00:14:39 +0200

    problema su ratais
    manau isvis nebuvo daryta wheels transform wheelrotation ar kaip jie ten
    vadinasi :)

--- Assets/Scripts/CarControleScript.js
+++ Assets/Scripts/CarControleScript.js
@@ -3,6 +3,10 @@ var wheelFL : WheelCollider;
 var wheelFR : WheelCollider;
 var wheelRL : WheelCollider;
 var wheelRR : WheelCollider;
+var wheelFLTrans : Transform;
+var wheelFRTrans : Transform;
+var wheelRLTrans : Transform;
+var wheelRRTrans : Transform;
 var maxTorque :  float = 50;
 
 function Start ()
@@ -16,4 +20,10 @@ function FixedUpdate ()
 	wheelRL.motorTorque = maxTorque * Input.GetAxis("Vertical");
 	wheelFL.steerAngle = 20 * Input.GetAxis("Horizontal");
 	wheelFR.steerAngle = 20 * Input.GetAxis("Horizontal");
+}
+function Update(){
+	wheelFLTrans.Rotate(wheelFL.rpm/60*360*Time.deltaTime,0,0);
+	wheelFRTrans.Rotate(wheelFR.rpm/60*360*Time.deltaTime,0,0);
+	wheelRLTrans.Rotate(wheelRL.rpm/60*360*Time.deltaTime,0,0);
+	wheelRRTrans.Rotate(wheelRR.rpm/60*360*Time.deltaTime,0,0);
 }
