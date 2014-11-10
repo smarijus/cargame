c1735abc626be109d65c828c61305b01567c087a
Marijus Sebeckis <smarijus@yahoo.com>
2014-11-10 23:27:46 +0200

    kazkas negerai su kamera nezinau kas

--- Assets/Scripts/CarCameraScript.js
+++ Assets/Scripts/CarCameraScript.js
@@ -5,13 +5,14 @@ var height : float = 1.4;
 var rotationDamping : float = 3.0;
 var heightDamping : float = 2.0;
 var zoomRacio : float = 0.5;
+var DefaultFOV : float = 60;
 private var rotationVector : Vector3;
 function Start () {
 
 }
 
 function LateUpdate () {
-var wantedAngel = car.eulerAngles.y;
+var wantedAngel = rotationVector.y;
 var wantedHeight = car.position.y + height;
 var myAngel = transform.eulerAngles.y;
 var myHeight = transform.position.y;
@@ -20,5 +21,17 @@ myHeight = Mathf.Lerp(myHeight, wantedHeight, heightDamping * Time.deltaTime);
 var currentRotation = Quaternion.Euler(0, myAngel,0);
 transform.position = car.position;
 transform.position -= currentRotation * Vector3.forward*distance;
+transform.position.y = myHeight;
 transform.LookAt(car);
+}
+
+function FixedUpadate(){
+var localVelocity = car.InverseTransformDirection(car.rigidbody.velocity);
+if(localVelocity.z < -0.5){
+rotationVector.y = car.eulerAngles.y + 180;
+}else{
+rotationVector.y = car.eulerAngles.y;
+}
+var acc = car.rigidbody.velocity.magnitude;
+camera.fieldOfView = DefaultFOV + acc*zoomRacio;
 }
