de9a3aca03592a5df90f7cf2401ef1dd952a205f
Programmer-M <wariuz.papildomas@gmail.com>
2014-11-25 17:44:44 +0200

    Padaryti padangų cypimo garsai
    Padaryti padangų cypimo garsai, esant padangų slydimui.

---
+++ Assets/Scripts/DestroyTimerScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyTimerScript : MonoBehaviour
+{
+
+
+	public float destroyAfter = 2;
+	private float timer;
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		timer += Time.deltaTime;
+		if (destroyAfter <= timer)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/SkiddingScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkiddingScript : MonoBehaviour
+{
+
+	public float currentFrictionValue;
+	private float skidAt = 1.5F;
+	public GameObject skidSound;
+	public float soundEmittion = 15;
+	private float soundWait;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		WheelHit hit;
+		transform.GetComponent<WheelCollider>().GetGroundHit(out hit);
+		currentFrictionValue = Mathf.Abs(hit.sidewaysSlip);
+		if (skidAt <= currentFrictionValue && soundWait <= 0)
+		{
+			Instantiate(skidSound, hit.point, Quaternion.identity);
+			soundWait = 1;
+		}
+		soundWait -= Time.deltaTime * soundEmittion;
+	}
+}
